b0bc661d422af54e378e48bdff6dea17f1d88070
livindra <livindradwiaditya@gmail.com>
2026-04-28 20:43:49 +0700

    setup_db

--- setup_db.py
+++ setup_db.py
@@ -13,7 +13,7 @@ from pymysql import MySQLError
 load_dotenv()
 
 DB_HOST = os.environ.get('DB_HOST', 'localhost')
-DB_PORT = int(os.environ.get('DB_PORT', '3306'))
+DB_PORT = os.environ.get('DB_PORT', '3306')
 DB_USER = os.environ.get('DB_USER', 'root')
 DB_PASSWORD = os.environ.get('DB_PASSWORD', '')
 DB_NAME = os.environ.get('DB_NAME', 'deteksi_pmk')
